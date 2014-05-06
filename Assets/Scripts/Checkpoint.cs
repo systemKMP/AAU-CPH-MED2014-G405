2ade7e4566628050160d576433fc71ada611b488
Tewlip <shartm12@student.aau.dk>
2014-05-06 10:22:21 +0200

    Checkpoint thingy.
    Moves spawnPoint to the checkpoint that has been triggered by the object tagged as player

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+    public Transform spawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") //looks for objects tagged as "Player"
+        {
+            spawnPoint.position = new Vector3(transform.position.x, spawnPoint.position.y, transform.position.z); //Sets the new spawn point to whichever checkpoint object last tagged by the player
+            //Destroy(gameObject);
+        }
+    }
+}
